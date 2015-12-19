2951ca68dfb7785326f2e58251b78c10ac05373c
solikha <ekhawaelah@yahoo.co.id>
2015-12-20 04:12:00 +0700

    latihan perubahan

--- inc/sidebar-back.php
+++ inc/sidebar-back.php
@@ -103,6 +103,23 @@
                         <i class="icon-double-angle-right"></i> Transaksi
                     </a>
                 </li>
+				 <li>
+            <a href="#" class="dropdown-toggle">
+                <i class="icon-pencil"></i>
+                <span> Pembelian</span>
+
+                <b class="arrow icon-angle-down"></b>
+            </a>
+
+          
+        
+
+            <ul class="submenu">
+                <li>
+                    <a href="index.php?mod=invoice&pg=invoice_view">
+                        <i class="icon-double-angle-right"></i> Supplier
+                    </a>
+                </li>
 
             </ul>
         </li>
